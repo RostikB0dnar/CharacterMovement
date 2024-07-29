e1bc9d5f5cb492b1e584b400798b07aac3585924
RostikB0dnar <155552585+RostikB0dnar@users.noreply.github.com>
2024-07-29 20:08:15 +0300

    Add files via upload

---
+++ Bootstrap.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+public class Bootstrap : MonoBehaviour
+{
+    /*public PlayerLocomotion thirdPersonMovement;
+    public PlayerManager playerManager;
+    public PlayerInputManager playerInputManager;
+    public AnimatorManager animatorManager;
+    public PlayerLocomotionServiceLocator playerLocomotionServiceLocator;*/
+    
+}
